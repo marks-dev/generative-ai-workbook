d52380f33a7b6e511cf3e171457978b17161b6a3
t.tanji <t-tanji@marks-t.jp>
2026-05-30 22:32:08 +0900

    Set up Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+export default function (eleventyConfig) {
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+}
